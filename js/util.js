3cf9f41b196a1f42d63b0670ca9144f202d615c1
Greg Borenstein <greg.borenstein@gmail.com>
2014-12-26 23:55:27 -0500

    player names get set successfully

--- js/util.js
+++ js/util.js
@@ -1,9 +1,8 @@
 var Util = {
-  names : ["joe", "bob", "jane", "cat", "pam", "kit", "van", "lin", "alice", "fin"],
+  names : ["joe", "bob", "jane", "cat", "pam", "kit", "van", "lin", "alice", "fin", "mark"],
 
   getRandomName : function(){
     var idx = Math.floor(Math.random() * Util.names.length);
-    conosole.log(Util.names);
     return Util.names.splice(idx, 1)[0];
   },
   
