02d53d382747c755f7a0a3f4bf7e8c96964a1862
Greg Borenstein <greg.borenstein@gmail.com>
2014-12-27 23:59:20 -0500

    hide controls based on allowed moves

--- README.md
+++ README.md
@@ -4,9 +4,15 @@
 
 #### Player control
 
-* hide move inputs if resulting square would be out-of-bounds
+* Fix orientation of controls relative to cols/rows (i.e. right moves down ATM)
+* Show list of moves for all characters for the next round.
+* Prompt for new move choice at end of round
 
 #### Other
 
 * Identify squares with multiple characters that need dialog
 * Dialog hello world
+
+#### Questions?
+
+* Maybe player inputs destinations instead of step-by-step moves? Or we have a pathing assistant?
--- js/player.js
+++ js/player.js
@@ -11,6 +11,25 @@ Player.prototype.nextPosition = function(){
 	return this.nextPos;
 }
 
+Player.prototype.isMoveLegal = function(move){
+	result = true;
+	if(this.position.col == 0){
+		result = (move.col > -1);
+	}
+	if(this.position.col == Game.boardWidth-1){
+		result = (move.col < 1);
+	}
+
+	if(this.position.row == 0 ){
+		result = (move.row > -1);
+	}
+	if(this.position.row == Game.boardHeight-1 ){
+		result = (move.row < 1);
+	}
+
+	return result;
+}
+
 Player.prototype.move = function(){
 	this.position = this.nextPos;
 }
