ea370b31590debf412cadd423ac52e52e9d424c0
Greg Borenstein <greg.borenstein@gmail.com>
2015-02-19 11:23:55 -0500

    typo

--- app/routes/move-instructions.js
+++ app/routes/move-instructions.js
@@ -6,7 +6,7 @@ export default Ember.Route.extend({
   setupController : function(controller, model){
     GameManager.transitionTo("moveInstructions");
 
-    controller.set('instructions', Game.moveInstructions;
+    controller.set('instructions', Game.moveInstructions);
     controller.set('victims', Game.newShootingVictims());
   }
 });
