b47975375dd05a4771e759317b36e1c522b7954d
Greg Borenstein <greg.borenstein@gmail.com>
2015-04-13 12:56:54 -0400

    full instruction list playback working, but not hooked up

--- js/instructionPlayer.js
+++ js/instructionPlayer.js
@@ -1,33 +1,70 @@
 var InstructionPlayer = {
   soundPath : 'public/sounds',
+  colors : ["red", "green", "yellow", "blue","white","black", "pink", "orange","brown", "gray"],
   sounds : {},
+  instructions : [],
   loadSounds : function(){
-    for(var i = 0 ; i < Util.colors.length; i++){
-      this.sounds[Util.colors[i]] = {};
-      this.sounds[Util.colors[i]].hold = new Howl({
-        urls : [this.soundPath + "/" + Util.colors[i]+"_holds.mp3"]
+    for(var i = 0; i < this.colors.length; i++){
+      this.sounds[this.colors[i]] = {};
+      this.sounds[this.colors[i]].holds = new Howl({
+        urls : [this.soundPath + "/" + this.colors[i]+"_holds.mp3"]
       });
-      this.sounds[Util.colors[i]].move = new Howl({
-        urls : [this.soundPath + "/move_" + Util.colors[i]+".mp3"]
+      this.sounds[this.colors[i]].move = new Howl({
+        urls : [this.soundPath + "/move_" + this.colors[i]+".mp3"]
       });
 
       this.sounds.dirs = {};
       dirs = Object.keys(Util.moves);
-      for(var i = 0; i < dirs.length; i++){
-        if(dirs[i] != "hold"){
-          this.sounds.dirs[dirs[i]] = new Howl({
-            urls : [this.soundPath + "/" + dirs[i] + ".mp3"]
+      for(var j = 0; j < dirs.length; j++){
+        if(dirs[j] != "hold"){
+          this.sounds.dirs[dirs[j]] = new Howl({
+            urls : [this.soundPath + "/" + dirs[j] + ".mp3"]
           });
         }
       }
     }
   },
 
+  playInstructions : function(elems){
+    this.instructions = this.parseInstructions(elems);
+    this.playNextInstruction();
+  },
+
+  currInstruction : 0,
+  playNextInstruction : function(){
+    console.log("next instruction");
+    console.log(this.instructions[this.currInstruction]);
+    this.playInstruction(this.instructions[this.currInstruction], function(){
+    InstructionPlayer.currInstruction++;
+      if(InstructionPlayer.currInstruction < InstructionPlayer.instructions.length){
+        InstructionPlayer.playNextInstruction();
+      }
+    });
+  },
+
+  parseInstructions : function(elems){
+    return $("#moveInstructions li").map(function(){
+      return InstructionPlayer.parseInstruction($(this))
+    });
+  },
+
+  parseInstruction : function(e){
+    words = e.text().split(".")[0].split(" ");
+    return {move :  words[words.length - 1], color: words[words.length - 2].toLowerCase()};
+  },
+
   playInstruction : function(opts, callback){
     console.log(opts)
 
-    if(opts.move == "hold"){
-      this.sounds[opts.color].hold.play();
+
+    if(opts.move == "holds"){
+      this.sounds[opts.color].holds._onend = [];
+      this.sounds[opts.color].holds._onend.push(function(){
+        if(callback){
+          callback();
+        }
+      });
+      this.sounds[opts.color].holds.play();
     } else {
       this.sounds[opts.color].move._onend = [];
       this.sounds[opts.color].move._onend.push(function(){
